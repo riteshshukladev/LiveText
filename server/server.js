2d48721ea6ef8ddeeb6de824c9012d9d753996fc
RItesh <shuklaritesh5678@gmail.com>
2024-07-24 03:48:32 +0530

    Added the route handler code and the key generation utility

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from "cors";
 import http from "http"
-import { initializeSocket } from "./service-worker/socketService";
-import { connectDatabase } from "./config/database";
-
+import { initializeSocket } from "./service-worker/socketService.js";
+import { connectDatabase } from "./config/database.js";
+import roomRouter from "./routes/roomRoutes.js"
 
 
 const app = express();
@@ -12,12 +12,17 @@ const server = http.createServer(app);
 app.use(cors());
 app.use(express.json());
 
-
-initializeSocket();
-
 connectDatabase();
 
-app.use('/', roomRoutes);
+initializeSocket(server);
+
+
+app.use('/', roomRouter);
+
+const PORT = process.env.PORT || 4001;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 
 
